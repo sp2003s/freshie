fe83ecba9db26528a13eaff56cbedcda918593f8
sp2003s <90305276+sp2003s@users.noreply.github.com>
2021-09-08 02:17:53 -0400

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	char button;
+	cout<<"input a charecter:";
+	cin>>button;
+	
+	switch (button)
+	{
+		case 'a':
+			cout<<"hello"<<endl;
+			break;
+			case 'b':
+				cout<<"namaste"<<endl;
+				break;
+				case 'c':
+					cout<<"salut"<<endl;
+					break;
+					case 'd':
+						cout<<"hola"<<endl;
+						break;
+						case 'e':
+							cout<<"ciao"<<endl;
+							break;
+							
+							default:
+								cout<<"i am still learning more!"<<endl;
+								break;
+	}
+	
+	return 0;
+}
